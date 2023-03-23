e198815baad977a5ebfdc94512f7b37460fc9b33
Julius <zailongshi2013@gmail.com>
2023-03-23 15:48:21 +0800

    session manage feature

--- src/views/ChatGPT.vue
+++ src/views/ChatGPT.vue
@@ -8,10 +8,19 @@ import MessageMe from '../components/MessageMe.vue'
 export default {
   data() {
     return {
-      title: '正在输入...'
+      title: '等待输入...',
+      sessionId: -1,
+      sessions: [],
     }
   },
   methods: {
+    saveSessions(sessionId, message) {
+      this.sessions[sessionId] ?? this.sessions.push({ title: message.content, messages: [] })
+      const session = this.sessions[sessionId]
+      session.messages.push(message)
+      localStorage.setItem('sessions', JSON.stringify(this.sessions))
+    },
+
     onSend() {
       let msg = this.$refs.input_box.textContent
       this.$refs.input_box.textContent = ''
@@ -19,6 +28,8 @@ export default {
       if (msg.trim().length === 0) {
         return
       }
+
+      this.title = '正在发送...'
 
       const insertMessage = (content, bgColor) => {
         const node = document.createElement('div');
@@ -32,9 +43,13 @@ export default {
       const template = `<MessageMe msg="${msg} datetime="${moment().format('YYYY-MM-DD HH:mm:ss')}" />`;
       insertMessage(msg, "#555555")
 
+      const message = { "role": "user", "content": `${msg}` }
+      this.sessionId = this.sessionId === -1 ? this.sessions.length : this.sessionId
+      this.saveSessions(this.sessionId, message)
+
       const data = {
         "model": "gpt-3.5-turbo",
-        "messages": [{ "role": "user", "content": `${msg}` }]
+        "messages": this.sessions[this.sessionId].messages
       }
 
       const config = {
@@ -46,9 +61,12 @@ export default {
 
       axios.post('https://api.openai.com/v1/chat/completions', JSON.stringify(data), config)
         .then(response => {
+          this.title = "等待输入..."
           let data = response.data
           const template = `<MessageMe msg="${data} datetime="${moment().format('YYYY-MM-DD HH:mm:ss')}" />`;
-          insertMessage(data.choices[0].message.content.trim(), "#00000000")
+          const message = data.choices[0].message
+          this.saveSessions(this.sessionId, message)
+          insertMessage(message.content.trim(), "#00000000")
         })
         .catch(error => {
           console.log(error)
@@ -56,7 +74,7 @@ export default {
     }
   },
   mounted() {
-
+    this.sessions = JSON.parse(localStorage.getItem('sessions')) ?? []
   }
 }
 </script>
